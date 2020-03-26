bd45d912244b65a0fdb5392bc5ecc4f63f81857e
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-03-26 19:35:31 +0100

    Added SFML Directory Linking

---
+++ Input/SpecialIOGame/SpecialIOGame/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	const int WIN_WIDTH = 400;
+	const int WIN_HEIGHT = 240;
+
+	sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Special Output");
+}
