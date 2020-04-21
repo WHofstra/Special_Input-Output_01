f40711d04bfee80d862d44d9c9c30f4fb09f34b9
HBlueRainDrop <43329986+HBlueRainDrop@users.noreply.github.com>
2020-04-21 17:47:12 +0200

    Gyro/Processor3

---
+++ Input/H/gyro.ino
@@ -0,0 +1,62 @@
+#include "Wire.h"
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+const int MPU_ADDR = 0x68;
+int16_t accelerometer_x, accelerometer_y, accelerometer_z;
+int16_t gyro_x, gyro_y, gyro_z;
+int16_t temperature;
+
+char tmp_str[7];
+char* convert_int16_to_str(int16_t i) {
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+/*const char* ssid      = "Medialab";
+const char* password  = "Mediacollege";
+const char* server    = "29980.hosts2.ma-cloud.nl";
+const int docCapacity = JSON_OBJECT_SIZE(6);*/
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void loop() {
+  ard();
+  //wifi();
+}
+
+void ard(){
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 7*2, true);
+  
+  accelerometer_x = Wire.read()<<8 | Wire.read();
+  accelerometer_y = Wire.read()<<8 | Wire.read();
+  accelerometer_z = Wire.read()<<8 | Wire.read();
+  //temperature = Wire.read()<<8 | Wire.read();
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read();
+  gyro_z = Wire.read()<<8 | Wire.read();
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+  //Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+  delay(10);
+}
+
+//void wifi(){
+//  }
